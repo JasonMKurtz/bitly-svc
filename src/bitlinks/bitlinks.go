9e059aaed98ec6dd53eeddddd1ff75b788f09f76
Jason Kurtz <jason@jkurtz.net>
2020-12-16 14:23:45 -0500

    Add library to get all bitlinks created for a specific group id

---
+++ src/bitlinks/bitlinks.go
@@ -0,0 +1,66 @@
+package bitlinks
+
+import (
+	"encoding/json"
+	"fmt"
+
+	httphelper "../http"
+)
+
+type DeepLink struct {
+	Id          string `json:"guid"`
+	Bitlink     string `json:"bitlink"`
+	URI         string `json:"app_uri_path"`
+	InstallURL  string `json:"install_url"`
+	AppId       string `json:"app_guid"`
+	Os          string `json:"os"`
+	InstallType string `json:"install_type"`
+	Created     string `json:"created"`
+	Modified    string `json:"modified"`
+	BrandId     string `json:"brand_guid"`
+}
+
+type Link struct {
+	Created     string            `json:"created_at"`
+	Id          string            `json:"id"`
+	Link        string            `json:"link"`
+	CustomLinks []string          `json:"custom_bitlinks"`
+	URL         string            `json:"long_url"`
+	Title       string            `json:"title"`
+	Archived    bool              `json:"archived"`
+	CreatedBy   string            `json:"created_by"`
+	ClientId    string            `json:"client_id"`
+	Tags        []string          `json:"tags"`
+	Deeplinks   []DeepLink        `json:"deeplinks"`
+	References  map[string]string `json:"references"`
+}
+
+type Page struct {
+	Prev  string `json:"prev"`
+	Next  string `json:"next"`
+	Size  int    `json:"size"`
+	Page  int    `json:"page"`
+	Total int    `json:"total"`
+}
+
+type Bitlink struct {
+	Links      []Link `json:"links"`
+	Pagination Page   `json:"pagination"`
+}
+
+func GetBitlinks(groupId, token string) []string {
+	endpoint := fmt.Sprintf("https://api-ssl.bitly.com/v4/groups/%s/bitlinks", groupId)
+	bytes := httphelper.GetBytes(token, endpoint)
+
+	var bitlink Bitlink
+	if err := json.Unmarshal(bytes, &bitlink); err != nil {
+		panic(err)
+	}
+
+	var ids []string
+	for _, link := range bitlink.Links {
+		ids = append(ids, link.Id)
+	}
+
+	return ids
+}
